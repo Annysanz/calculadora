b96ed77268381da37357adcaaecf9afca391b0b1
Annysanz <annysanz17@gmail.com>
2022-01-31 09:16:09 +0100

    finish

--- js/main.js
+++ js/main.js
@@ -1,19 +1,23 @@
-const historico=[];
+const historical = [];
+
 function myFunction(id) {
     document.calc.result.value+=id;
   }
   function clearScreen() {
     document.calc.result.value="";
+    document.calc.historical.value=historical;
   }
   function calculate() {
     try {
-      var input = eval(document.calc.result.value);
-      historico.push(input)
-      console.log(historico)
-      document.calc.historico.value=historico;
+      let input = eval(document.calc.result.value);
+      historical.push(input)
+
       document.calc.result.value=input;
+      document.calc.historical.value=historical;
+      console.log(historical)
     } catch(err){
-        document.calc.result.value="Error";
+        document.calc.result.value="Error 😆";
       }
   }
 
+  
